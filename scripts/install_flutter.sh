6ba31f955867cbf90e5f1bff9d40c22fe8d3729c
mallipedhisandeep <mallipedhisandeep@gmail.com>
2026-05-22 00:08:52 +0530

    Add Flutter installation script

---
+++ scripts/install_flutter.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+git config --global --add safe.directory /vercel/path0
+git config --global --add safe.directory /opt/flutter
+curl -fsSL https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.32.0-stable.tar.xz | tar -xJ -C /opt
+export PATH=$PATH:/opt/flutter/bin
+flutter pub get
