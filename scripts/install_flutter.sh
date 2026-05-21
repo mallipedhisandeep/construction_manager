de552a1ccb0e3d51c508691fe16950b2af31787c
mallipedhisandeep <mallipedhisandeep@gmail.com>
2026-05-22 00:14:56 +0530

    Update Flutter version to 3.44.0 in install script

--- scripts/install_flutter.sh
+++ scripts/install_flutter.sh
@@ -2,6 +2,6 @@
 set -e
 git config --global --add safe.directory /vercel/path0
 git config --global --add safe.directory /opt/flutter
-curl -fsSL https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.32.0-stable.tar.xz | tar -xJ -C /opt
+curl -fsSL https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.44.0-stable.tar.xz | tar -xJ -C /opt
 export PATH=$PATH:/opt/flutter/bin
 flutter pub get
